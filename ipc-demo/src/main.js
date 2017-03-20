5c1ff5a4de5c23c9cbb7d473326fcc74f10a2034
Vasyl <vasyl.shumskyi@gmail.com>
2017-03-20 15:33:30 +0200

    10 - Try out Electron - Countdown Demo - Multi Windows

--- ipc-demo/src/main.js
+++ ipc-demo/src/main.js
@@ -6,34 +6,38 @@ const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 const ipc = electron.ipcMain                    // we need to import ipcMain
 
-let mainWindow
+const windows = []
 
 app.on('ready', _ => {
-  mainWindow = new BrowserWindow({
-    height: 400,
-    width: 400
+
+  [1,2,3].forEach(_ => {
+
+      let win = new BrowserWindow({
+        height: 400,
+        width: 400
+      })
+
+      win.loadURL(`file://${__dirname}/countdown.html`)
+
+      win.on('closed', _ => {
+        console.log('closed!');
+        mainWindow = null
+      })
+
+      windows.push(win)             // ?
+
   })
-
-  mainWindow.loadURL(`file://${__dirname}/countdown.html`)
-
-// countdown()
-
-// Open the DevTools.
-// mainWindow.webContents.openDevTools()
-
-  mainWindow.on('closed', _ => {
-    console.log('closed!');
-    mainWindow = null
-  })
-
 })
 
 // main module must listen for coundown-start and capture it
 ipc.on('countdown-start', _ => {
   //console.log("main module caught it!");
-// we need to call coundown, caught the count and send it back to the renderer module
+  // we need to call coundown, caught the count and send it back to the renderer module
   countdown(count => {
       console.log('count', count)
-      mainWindow.webContents.send('countdown', count)     // webContents is event emitor instance
+
+      windows.forEach(win => {
+          win.webContents.send('countdown', count)     // webContents is event emitor instance
+      })
   })
 })
